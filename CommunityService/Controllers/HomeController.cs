75e3bcb8e4a0b9c70c75973947da76db6d3e3923
Alexandra Saldan <alexsaldana9@gmail.com>
2018-10-10 13:15:19 -0400

    first commit

---
+++ CommunityService/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommunityService.Models;
+
+namespace CommunityService.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Greeting = "Hello, here are events where you can help";
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ViewResult ListofEvents()
+        {
+            return View();
+        }
+
+        public ViewResult ListofOrganizations()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult SignUpForEvent(GuestResponse guestResponse)
+        {
+            // TODO: Email response to the party organizer
+            //return View("Thanks", guestResponse);
+
+            return View("Thanks", guestResponse);
+        }
+    }
+}
---
+++ CommunityService/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommunityService.Startup))]
+namespace CommunityService
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
